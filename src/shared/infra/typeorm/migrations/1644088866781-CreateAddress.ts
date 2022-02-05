6ef40f60f4bb698dcf871b16f4daeb46bfa997a4
Marcel Brasileiro <brasileirodev@gmail.com>
2022-02-05 16:39:46 -0300

    Criação das migrations de criação das tabelas

---
+++ src/shared/infra/typeorm/migrations/1644088866781-CreateAddress.ts
@@ -0,0 +1,70 @@
+import {
+  MigrationInterface, QueryRunner, Table, TableForeignKey,
+} from 'typeorm';
+
+export class CreateAddress1644088866781 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'address',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'country',
+            type: 'varchar',
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+          },
+          {
+            name: 'address',
+            type: 'varchar',
+          },
+          {
+            name: 'complete_address',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+    await queryRunner.createForeignKey('address', new TableForeignKey({
+      name: 'AddressOwner',
+      columnNames: ['user_id'],
+      referencedColumnNames: ['id'],
+      referencedTableName: 'users',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('address', 'AddressOwner');
+    await queryRunner.dropTable('address');
+  }
+}
