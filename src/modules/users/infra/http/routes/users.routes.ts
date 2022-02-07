25b801e8afd54d5063995f59efd855e24d221558
Marcel Brasileiro <brasileirodev@gmail.com>
2022-02-07 19:24:26 -0300

    Ativando middleware de check authentication

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
+import ensureAuhenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
 const usersController = new UsersController();
 
 usersRouter.post('/', usersController.create);
-usersRouter.get('/:id', usersController.find);
-usersRouter.put('/:id', usersController.update);
-usersRouter.delete('/:id', usersController.delete);
+usersRouter.get('/:id', ensureAuhenticated, usersController.find);
+usersRouter.put('/:id', ensureAuhenticated, usersController.update);
+usersRouter.delete('/:id', ensureAuhenticated, usersController.delete);
 
 export default usersRouter;
--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import addressesRouter from '@modules/addresses/infra/http/routes/addresses.routes';
+import ensureAuhenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
-routes.use('/addresses', addressesRouter);
+routes.use('/addresses', ensureAuhenticated, addressesRouter);
 
 export default routes;
