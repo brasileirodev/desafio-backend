ce24eac77434b50458e3561ff0869722b841f530
Marcel Brasileiro <brasileirodev@gmail.com>
2022-02-08 21:43:00 -0300

    Criando os metodos delete e update do FakeUsersRepository

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -2,6 +2,7 @@ import { v4 } from 'uuid';
 import User from '@modules/users/infra/typeorm/entities/User';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUpdateUserDTO from '@modules/users/dtos/IUpdateUserDTO';
 
 class UsersRepository implements IUsersRepository {
   private users: User[] = [];
@@ -26,6 +27,19 @@ class UsersRepository implements IUsersRepository {
 
   public async save(user: User): Promise<User> {
     this.users.push(user);
+    return user;
+  }
+
+  public async delete(id: string): Promise<void> {
+    this.users = this.users.filter((user) => user.id !== id);
+  }
+
+  public async update(data: IUpdateUserDTO): Promise<User> {
+    const user = await this.findById(data.id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    Object.assign(user, { ...data });
     return user;
   }
 }
