b36689ccd81c689d30811b7d4985041af2dbcc67
Marcel Brasileiro <brasileirodev@gmail.com>
2022-02-09 15:20:11 -0300

    update documentation

--- src/modules/addresses/infra/http/routes/addresses.routes.ts
+++ src/modules/addresses/infra/http/routes/addresses.routes.ts
@@ -5,8 +5,8 @@ const addressesRouter = Router();
 const addressesController = new AddressesController();
 
 addressesRouter.post('/', addressesController.create);
-addressesRouter.delete('/:id', addressesController.delete);
 addressesRouter.get('/', addressesController.index);
+addressesRouter.delete('/:id', addressesController.delete);
 addressesRouter.get('/:id', addressesController.find);
 addressesRouter.put('/:id', addressesController.update);
 
